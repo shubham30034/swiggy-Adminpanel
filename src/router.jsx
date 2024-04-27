971ff4256567229929736d2796e1be7a97bc0f80
shubham kundu <shubhamkundu758@gmail.com>
2024-04-27 18:32:27 +0530

    addDoahboardLayout

--- src/router.jsx
+++ src/router.jsx
@@ -2,13 +2,25 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/LoginPage";
 import Homepage from "./pages/Homepage";
 import Register from "./pages/RegisterPage";
+import Dashboardlayout from "./layouts/DashboardLayout";
+import Dishes from "./pages/Dishes";
 
 
 const router = createBrowserRouter([
 
     {
         path:'/',
-        element:<Homepage/>
+        element:<Dashboardlayout/>,
+        children:[
+            {
+                path:"home",
+                element : <Homepage/>
+            },
+            {
+                element:"dishes",
+                element:<Dishes/>
+            }
+        ]
     },
     {
         path:'/login',
